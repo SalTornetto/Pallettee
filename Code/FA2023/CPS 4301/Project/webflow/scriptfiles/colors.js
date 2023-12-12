8e8a9cd06773dcfdcd8ff8797a805b00400f77d0
Mo <mohammadnassrallah17@gmail.com>
2023-12-12 04:26:18 -0500

    fixed copy, added rgb cmyk, fixed bugs

--- Code/FA2023/CPS 4301/Project/webflow/scriptfiles/colors.js
+++ Code/FA2023/CPS 4301/Project/webflow/scriptfiles/colors.js
@@ -53,10 +53,21 @@ generateColorPalette = () => {
     reader.readAsDataURL(input.files[0]);
   }
 };
+clearContent = (elementId) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.innerHTML = "";
+  }
+};
+
+// Usage
+
 
 pickColorFromImage = () => {
   const uploadedImage = document.getElementById("uploadedImage");
-  const colorValue = document.getElementById("hex-code");
+  const hexval = document.getElementById("hex-code");
+  const rgbval = document.getElementById("rgb-code");
+  const cmykval = document.getElementById("cmyk-code");
 
   uploadedImage.addEventListener("click", function (event) {
     const canvas = document.createElement("canvas");
@@ -75,19 +86,41 @@ pickColorFromImage = () => {
     const y = event.offsetY;
     const pixelData = context.getImageData(x, y, 1, 1).data;
     const hexColor = rgbToHex(pixelData);
-
-    // Remove the last clicked value
-    const previousHexCode = document.getElementById("hex-code");
-    if (previousHexCode) {
-      previousHexCode.innerHTML = ""; // Clear the content
-    }
+    const rgbColor = `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`;
+    const cmykColor = rgbToCmyk(pixelData);
+    clearContent("hex-code");
+    clearContent("rgb-code");
+    clearContent("cmyk-code");
 
     // Display the new hex code with a class for styling
     const hexCode = document.createElement("span");
     hexCode.innerText = hexColor;
-    hexCode.classList.add("tabbed-hex"); // Add a class for styling
-    colorValue.appendChild(hexCode);
+    hexval.appendChild(hexCode);
+
+    // Display the new rgb code
+    const rgbCode = document.createElement("span");
+    rgbCode.innerText = rgbColor;
+    rgbval.appendChild(rgbCode);
+
+    // Display the new cmyk code
+    const cmykCode = document.createElement("span");
+    cmykCode.innerText = cmykColor;
+    cmykval.appendChild(cmykCode);
   });
+};
+
+// Convert RGB to CMYK
+rgbToCmyk = (pixelData) => {
+  const r = pixelData[0] / 255;
+  const g = pixelData[1] / 255;
+  const b = pixelData[2] / 255;
+
+  const k = 1 - Math.max(r, g, b);
+  const c = (1 - r - k) / (1 - k);
+  const m = (1 - g - k) / (1 - k);
+  const y = (1 - b - k) / (1 - k);
+
+  return `(${Math.round(c * 100)}, ${Math.round(m * 100)}, ${Math.round(y * 100)}, ${Math.round(k * 100)})`;
 };
 
 // Modify the rgbToHex function to directly accept pixelData
@@ -99,35 +132,36 @@ rgbToHex = (pixelData) => {
 };
 
 copyToClipboard = (codeType) => {
-    let codeElement;
-  
-    switch (codeType) {
-      case 'hex':
-        codeElement = document.getElementById('hex-value');
-        break;
-      case 'rgb':
-        codeElement = document.getElementById('rgb-value');
-        break;
-      case 'cmyk':
-        codeElement = document.getElementById('cmyk-value');
-        break;
-      default:
-        return; // If codeType is not recognized, do nothing
-    }
-  
-    if (codeElement) {
-      const codeValue = codeElement.innerText;
-  
-      // Use navigator.clipboard.writeText for modern browsers
-      navigator.clipboard
-        .writeText(codeValue)
-        .then(() => {
-          // Provide user feedback (you can customize this part)
-          alert(`Text copied to clipboard: ${codeValue}`);
-        })
-        .catch((err) => {
-          console.error("Unable to copy to clipboard", err);
-        });
-    }
-  };
-  
+  let codeElement;
+
+  switch (codeType) {
+    case "hex-value":
+      codeElement = document.getElementById("hex-code");
+      break;
+    case "rgb-value":
+      codeElement = document.getElementById("rgb-code");
+      break;
+    case "cmyk-value":
+      codeElement = document.getElementById("cmyk-code");
+      break;
+    default:
+      return; // If codeType is not recognized, do nothing
+  }
+
+  if (codeElement) {
+    const codeValue = codeElement.innerText;
+
+    // Use navigator.clipboard.writeText for modern browsers
+    navigator.clipboard
+      .writeText(codeValue)
+      .then(() => {
+        // Provide user feedback (you can customize this part)
+        alert(`Text copied to clipboard: ${codeValue}`);
+      })
+      .catch((err) => {
+        console.error("Unable to copy to clipboard", err);
+      });
+  }
+};
+
+
