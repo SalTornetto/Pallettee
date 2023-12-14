a8292c5526adde35fa71246f61d1c279d9dbb416
Sal Tornetto <saltornetto1@gmail.com>
2023-12-13 22:12:47 -0500

    implemented new color picking algorithm

--- Code/FA2023/CPS 4301/Project/webflow/scriptfiles/color2.js
+++ Code/FA2023/CPS 4301/Project/webflow/scriptfiles/color2.js
@@ -1,9 +1,8 @@
 generateColorPalette = () => {
-    console.log('hello world');
     const input = document.getElementById('imageInput');
     const colorPalette = document.getElementById('colorPalette');
     const uploadedImage = document.getElementById('uploadedImage');
-    //colorPalette.innerHTML = ''; // Clear previous colors
+    colorPalette.innerHTML = ''; // Clear previous colors
   
     if (input.files && input.files[0]) {
         const reader = new FileReader();
@@ -22,23 +21,18 @@ generateColorPalette = () => {
                 context.drawImage(img, 0, 0, img.width, img.height);
   
                 const imageData = context.getImageData(0, 0, img.width, img.height).data;        
-                //this is an array that has 4 values for every pixel
-                // console.log("imageData:");
-                // console.log(imageData);  
+
+
                 const colorCount = 5; // Number of colors to extract
   
               
                 const colorMap = groupRgba(imageData);
   
-  
                 let result = kmeans(colorMap, colorCount);
-                
-  console.log(result);
-  // console.log(result.centroids[0]); 
-  // console.log("||||||||||||||||||||||||||||||||||||");
-  
+   
                 const sortedColors = Object.keys(colorMap).sort((a, b) => colorMap[b] - colorMap[a]);
   
+
                 for (let i = 0; i < colorCount; i++) {
                     const hexColor = numbersToHex(result.centroids[i]);
                     console.log(typeof(hexColor));
@@ -48,8 +42,8 @@ generateColorPalette = () => {
                     colorBox.style.width = Math.round(100/colorCount)+"%";
                     colorBox.innerText = hexColor;
                     colorPalette.appendChild(colorBox); 
-
                 }
+                
                 
             };
         };
@@ -58,53 +52,9 @@ generateColorPalette = () => {
     }
   };
   
-  // function pickColorFromImage() {
-  //   const uploadedImage = document.getElementById('uploadedImage');
-  //   const canvas = document.createElement('canvas');
-  //   const context = canvas.getContext('2d');
-  //   canvas.width = uploadedImage.width;
-  //   canvas.height = uploadedImage.height;
-  //   context.drawImage(uploadedImage, 0, 0, uploadedImage.width, uploadedImage.height);
-  
-  //   const colorPickerCanvas = document.createElement('canvas');
-  //   colorPickerCanvas.width = 1;
-  //   colorPickerCanvas.height = 1;
-  //   const colorPickerContext = colorPickerCanvas.getContext('2d');
-  
-  //   document.body.appendChild(colorPickerCanvas);
-  
-  //   colorPickerCanvas.addEventListener('click', function (event) {
-  //       const x = event.clientX - colorPickerCanvas.getBoundingClientRect().left;
-  //       const y = event.clientY - colorPickerCanvas.getBoundingClientRect().top;
-  
-  //       const imageData = context.getImageData(x, y, 1, 1).data;
-  //       const color = `rgb(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`;
-  //       const hexColor = rgbToHexSAL(color);
-  
-  //       const colorBox = document.createElement('div');
-  //       colorBox.className = 'colorBox';
-  //       colorBox.style.backgroundColor = hexColor;
-  //       colorBox.innerText = hexColor;
-  
-  //       const colorPalette = document.getElementById('colorPalette');
-  //       colorPalette.appendChild(colorBox);
-  
-  //       document.body.removeChild(colorPickerCanvas);
-  //   });
-  
-  //   colorPickerCanvas.style.position = 'absolute';
-  //   colorPickerCanvas.style.left = '0';
-  //   colorPickerCanvas.style.top = '0';
-  //   colorPickerCanvas.style.pointerEvents = 'none';
-  
-  //   colorPickerContext.drawImage(uploadedImage, 0, 0, 1, 1);
-  
-  //   colorPickerCanvas.click();
-  // }
   
   function rgbToHexSAL(rgb) {
     const match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    console.log('Is this being called?')
     if (!match) return rgb;
   
     function hex(x) {
@@ -152,60 +102,7 @@ generateColorPalette = () => {
     b = b * a + 255 * (1-a);
     return [r, g, b];
   }
-  // generateColorPalette = () => {
-  //   const input = document.getElementById("imageInput");
-  //   const colorPalette = document.getElementById("colorPalette");
-  //   const uploadedImage = document.getElementById("uploadedImage");
   
-  //   if (input.files && input.files[0]) {
-  //     const reader = new FileReader();
-  
-  //     reader.onload = function (e) {
-  //       uploadedImage.src = e.target.result;
-  
-  //       const img = new Image();
-  //       img.src = e.target.result;
-  
-  //       img.onload = function () {
-  //         const canvas = document.createElement("canvas");
-  //         const context = canvas.getContext("2d");
-  //         canvas.width = img.width;
-  //         canvas.height = img.height;
-  //         context.drawImage(img, 0, 0, img.width, img.height);
-  
-  //         const imageData = context.getImageData(
-  //           0,
-  //           0,
-  //           img.width,
-  //           img.height
-  //         ).data;
-  //         const colorCount = 10; // Number of colors to extract
-  
-  //         // Extract dominant colors using a simple algorithm
-  //         const colorMap = {};
-  //         for (let i = 0; i < imageData.length; i += 4) {
-  //           const color = `rgb(${imageData[i]}, ${imageData[i + 1]}, ${
-  //             imageData[i + 2]
-  //           })`;
-  //           colorMap[color] = (colorMap[color] || 0) + 1;
-  //         }
-  
-  //         const sortedColors = Object.keys(colorMap).sort(
-  //           (a, b) => colorMap[b] - colorMap[a]
-  //         );
-  
-  //         for (let i = 0; i < Math.min(colorCount, sortedColors.length); i++) {
-  //           const hexColor = rgbToHex(sortedColors[i]);
-  //         }
-  
-  //         // Set the flex-direction to column for vertical rectangles
-  //         colorPalette.style.flexDirection = "column";
-  //       };
-  //     };
-  
-  //     reader.readAsDataURL(input.files[0]);
-  //   }
-  // };//end generate pallete method
   
   
   
@@ -216,28 +113,28 @@ generateColorPalette = () => {
     }
   };
   
+  //temperarily comented out thius code to reduce lag
+  // const hasSupport = () => ('EyeDropper' in window);
   
-//   const hasSupport = () => ('EyeDropper' in window);
+  // const imageElement = document.getElementById("uploadedImage");
   
-//   const imageElement = document.getElementById("uploadedImage");
+  // imageElement.addEventListener("mouseover", () => {
+  //   if (hasSupport) {
+  //     const eyeDropper = new window.EyeDropper();
   
-//   imageElement.addEventListener("mouseover", () => {
-//     if (hasSupport) {
-//       const eyeDropper = new window.EyeDropper();
-  
-//       eyeDropper
-//         .open()
-//         .then((result) => {
-//           const color = result.sRGBHex;
-//           // Do something with the color
-//         })
-//         .catch((e) => {
-//           console.error(e);
-//         });
-//     } else {
-//       console.warn("No Support: This browser does not support the EyeDropper API yet!");
-//     }
-//   });
+  //     eyeDropper
+  //       .open()
+  //       .then((result) => {
+  //         const color = result.sRGBHex;
+  //         // Do something with the color
+  //       })
+  //       .catch((e) => {
+  //         console.error(e);
+  //       });
+  //   } else {
+  //     console.warn("No Support: This browser does not support the EyeDropper API yet!");
+  //   }
+  // });
   
   pickColorFromImage = () => {
     const uploadedImage = document.getElementById("uploadedImage");
